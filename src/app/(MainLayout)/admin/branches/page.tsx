2781c51ad004bec9b8ea35c7245438a1b8d2b42f
tonoysharma <tonoyshaema58@gmail.com>
2026-08-21 23:29:21 +0600

    added deshboard

---
+++ src/app/(MainLayout)/admin/branches/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const BranchesPage = () => {
+    return (
+        <div className="p-4 text-2xl font-bold text-center">
+            <h1>Branches Page</h1>
+            <p>Welcome to the Branches Page!</p>
+        </div>
+    );
+};
+
+export default BranchesPage;
