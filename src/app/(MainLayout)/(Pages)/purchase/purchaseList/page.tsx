a253e60360f8210b7f6d8ed577ab30bcf446982c
tonoysharma <tonoyshaema58@gmail.com>
2026-08-26 20:01:18 +0600

    added purches page and solve path name

---
+++ src/app/(MainLayout)/(Pages)/purchase/purchaseList/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const PurchaseListPage = () => {
+    return (
+        <div>
+            <h1>Purchase List</h1>
+        </div>
+    );
+};
+
+export default PurchaseListPage;
