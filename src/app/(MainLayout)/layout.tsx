9e6b8026f319eb20b79c836c6b8103cc4c02d374
tonoysharma <tonoyshaema58@gmail.com>
2026-09-13 15:43:29 +0600

    fix support widget

--- src/app/(MainLayout)/layout.tsx
+++ src/app/(MainLayout)/layout.tsx
@@ -35,7 +35,9 @@ import SystemConfigPage from "@/app/(MainLayout)/(Pages)/admin/system-config/pag
 import NotFoundPage from "@/app/(MainLayout)/not-found-page";
 import ComplaintReceviedPage from "./(Pages)/rma/complain-received/page";
 import AIInsightsPage from "@/app/(MainLayout)/(Pages)/ai-insights/page";
+
 import PlansPage from "./(Pages)/admin/Plans/page";
+import ModernSupportWidget from "@/components/Support/ModernSupport";
 
 
 
@@ -90,6 +92,10 @@ const MainLayout = () => {
           <DefaultHeader />
           <PageRenderer routeMap={ROUTE_MAP} />
         </div>
+
+        {/* support widget */}
+        <ModernSupportWidget />
+
       </div>
     </TabProvider>
   );
