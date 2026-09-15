f73acae250749f2d070089ee7b503003507bccf3
Ashik2630 <ashik426370@gmail.com>
2026-09-15 23:19:35 +0600

    Update somthing

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -8,7 +8,7 @@ import { MongoClient } from "mongodb";
 import { mongodbAdapter } from "better-auth/adapters/mongodb";
 import { jwt } from "better-auth/plugins";
 
-const client = new MongoClient(process.env.MONGODB_URI as string);
+const client = new MongoClient(process.env.MONGODB_URL as string);
 const db = client.db("tech_basket");
 
 export const auth = betterAuth({
