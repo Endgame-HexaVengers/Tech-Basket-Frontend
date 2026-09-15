620620ecc3ee66033d163d5583796c7139af8a03
tonoysharma <tonoyshaema58@gmail.com>
2026-09-16 01:31:41 +0600

    change client name

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,17 +1,18 @@
 import dns from "node:dns/promises";
+
 dns.setServers(["8.8.8.8", "8.8.4.4"]);
-
-
 
 import { betterAuth } from "better-auth";
 import { MongoClient } from "mongodb";
 import { mongodbAdapter } from "better-auth/adapters/mongodb";
 import { jwt } from "better-auth/plugins";
 
-const client = new MongoClient(process.env.MONGODB_URI as string);
-const db = client.db("tech_basket");
+const client = new MongoClient(process.env.MONGODB_URL as string);
+const db = client.db("TechBasket");
 
 export const auth = betterAuth({
+  secret: process.env.BETTER_AUTH_SECRET,
+
   database: mongodbAdapter(db, {
     client,
   }),
@@ -43,7 +44,7 @@ export const auth = betterAuth({
     cookieCache: {
       enabled: true,
       strategy: "jwt",
-      maxAge: 20 * 24 * 60 * 60,
+      maxAge: 30 * 24 * 60 * 60,
     },
   },
 
